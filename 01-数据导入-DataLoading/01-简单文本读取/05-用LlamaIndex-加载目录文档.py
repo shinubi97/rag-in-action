c4f2eb986433e0edcff30c9be33d16e96718e2fb
FlyAIBox <fly910905@sina.com>
2025-06-11 16:34:57 +0800

    【U】修复路径

--- 01-数据导入-DataLoading/01-简单文本读取/05-用LlamaIndex-加载目录文档.py
+++ 01-数据导入-DataLoading/01-简单文本读取/05-用LlamaIndex-加载目录文档.py
@@ -7,7 +7,7 @@ print(f"文档数量: {len(documents)}")
 print(documents[0].text[:100])  # 打印第一个文档的前100个字符
 
 # 仅加载某一个特定文件
-dir_reader = SimpleDirectoryReader(input_files=["data/黑神话/黑神话悟空的设定.txt"])
+dir_reader = SimpleDirectoryReader(input_files=["90-文档-Data/黑悟空/设定.txt"])
 documents = dir_reader.load_data()
 print(f"文档数量: {len(documents)}")
 print(documents[0].text[:100])  # 打印第一个文档的前100个字符
