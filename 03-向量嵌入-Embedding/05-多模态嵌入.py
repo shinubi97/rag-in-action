f3b2f424a58be62b8d522e766aadb7bf7bead36c
FlyAIBox <fly910905@sina.com>
2025-06-14 17:53:12 +0800

    【U】多模态嵌入优化

--- 03-向量嵌入-Embedding/05-多模态嵌入.py
+++ 03-向量嵌入-Embedding/05-多模态嵌入.py
@@ -2,6 +2,8 @@
 多模态嵌入简单示例：使用Visualized-BGE模型对图片进行编码
 安装visual_bge，参考：
 https://github.com/FlagOpen/FlagEmbedding/tree/master/research/visual_bge#readme
+
+# 如果下载安装visual_bge后，依然无法使用，请切换到物理环境使用，虚拟环境可能无法使用
 """
 
 import torch
@@ -11,12 +13,15 @@ import numpy as np
 
 # 初始化编码器
 model_name = "BAAI/bge-base-en-v1.5"
-model_path = "./Visualized_base_en_v1.5.pth"
+# 定义模型路径（如果有问题，请使用绝对路径）
+# 请提前下载模型权重文件
+# wget https://huggingface.co/BAAI/bge-visualized/resolve/main/Visualized_base_en_v1.5.pth
+model_path = "/root/AI-BOX/code/rag/rag-in-action/03-向量嵌入-Embedding/Visualized_base_en_v1.5.pth"
 model = Visualized_BGE(model_name_bge=model_name, model_weight=model_path)
 model.eval()
 
-# 定义图片路径
-image_path = "90-文档-Data/多模态/query_image.jpg"
+# 定义图片路径（如果有问题，请使用绝对路径）
+image_path = "/root/AI-BOX/code/rag/rag-in-action/90-文档-Data/多模态/query_image.jpg"
 
 # 对图片进行编码
 with torch.no_grad():
